67165f079dc8408a339f8f4863687f9b878c96f3
Matt Brigida <mdbrigida@gmail.com>
2019-07-24 13:41:03 -0400

    command line script version done

--- data_cleaning_and_aggregation_scripts/fetch_and_merge.py
+++ data_cleaning_and_aggregation_scripts/fetch_and_merge.py
@@ -1,10 +1,36 @@
+#!/usr/bin/python
+
 ### Fetch data from FDIC and merge all files------
 
 import pandas as pd
 import os
+import sys
+import urllib.request
+import glob
+import zipfile
 
-quarter = '20190331'
+quarter = sys.argv[1]
 
+## remove all files from ./raw_data/ and download quarter into folder, extract zip, and remove readme file and zip folder-------
+
+### remove all files in ./raw_data/
+files_to_delete = glob.glob("./raw_data/*")
+for ff in files_to_delete:
+    os.remove(ff)
+
+
+### download zip for quarter
+urllib.request.urlretrieve("https://www5.fdic.gov/sdi/Resource/AllReps/All_Reports_"+ quarter +".zip", "./raw_data/" + quarter + "data.zip")
+
+### unzip
+with zipfile.ZipFile("./raw_data/" + quarter + "data.zip","r") as zip_ref:
+    zip_ref.extractall("./raw_data")
+
+### remove zip file and readme
+os.remove("./raw_data/" + quarter + "data.zip")
+os.remove("./raw_data/All_Reports_" + quarter + "_readme.htm")
+
+### make new merged data file---
 os.mkdir("./merged_data/"+ quarter +"/")
 
 ## Plan: get list of IDRSSDs from one file and then use loop to merge in the rest of the files.
@@ -22,14 +48,7 @@ bank_data = pd.DataFrame(PD_NA[columns_to_drop_plus_fed_rssd])
 
 data = pd.DataFrame(PD_NA['fed_rssd'])
 
-# FRNLL = pd.read_csv("./raw_data/All_Reports_"+ quarter +"_1-4 Family Residential Net Loans and Leases.csv")
-
 ## game plan:  take all repetitive data and put into one file.  then on read for each table remove all the repetitive data except fed_rssd and merge
-
-# PD_NA = PD_NA.drop(columns_to_drop, axis=1)
-# FRNLL = FRNLL.drop(columns_to_drop, axis=1)
-
-# data = pd.merge(PD_NA, FRNLL, on='fed_rssd')
 
 ### read new data and merge--------
 
